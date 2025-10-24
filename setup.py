bd02dc507dc866595b99148e7287562581cee27d
Darshan Ganorkar <ganorkardarshan264@gmail.com>
2025-10-24 10:35:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     name="sensor",
     version="0.0.1",
     author="ineuron",
-    author_email="princekatiyar986@gmail.com",
+    author_email="ganorkardarshan264@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
